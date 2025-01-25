2e01c5abab1ef4fefad6f4933117286e1a251ab9
mohammad khajehnoori <mohammad.khajehnoori.05@gmail.com>
2025-01-25 22:00:54 +0330

    first commit (creating initial files)

---
+++ src/ds_final_project_huffman/Ds_final_Project_huffman.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ds_final_project_huffman;
+
+/**
+ *
+ * @author Asus
+ */
+public class Ds_final_Project_huffman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
